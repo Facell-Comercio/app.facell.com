6686d6a80e0d056cc2125160f31a2a893e984d17
Alex <14alex25@gmail.com>
2024-03-31 11:56:38 -0300

    feat: add FormFileUpload, FileUpload, UploadDropzone

---
+++ src/types/user.ts
@@ -0,0 +1,12 @@
+type UserType = {
+    id: string,
+    nome: string,
+    ativo: number
+    email: string
+    id_grupo_economico: any
+    id_perfil: number
+    img_url: string
+    nivel_acesso: string
+    perfil: string
+    roles: string
+}
