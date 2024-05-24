fceea78165288f35d6a8be1d4c36a7ce0c1ec356
jonathan <jonathanamarante@facell.com>
2024-05-24 14:56:44 -0300

    fix: paginação do painel

--- src/hooks/financeiro/usePainel.ts
+++ src/hooks/financeiro/usePainel.ts
@@ -7,12 +7,12 @@ import { keepPreviousData, useQuery } from "@tanstack/react-query";
 export const usePainel = () => {
     return ({
         getAllSolicitacoesNegadas : ({ pagination }: GetAllParams) => useQuery({
-            queryKey: ['fin_painel_negados'],
+            queryKey: ['fin_painel_negados', pagination],
             queryFn: async () => { return await api.get(`/financeiro/contas-a-pagar/painel/negados`, { params: { pagination } }) },
             placeholderData: keepPreviousData
         }),
         getAllNotasFiscaisPendentes : ({ pagination }: GetAllParams) => useQuery({
-            queryKey: ['fin_painel_sem_nota'],
+            queryKey: ['fin_painel_sem_nota', pagination],
             queryFn: async () => { return await api.get(`/financeiro/contas-a-pagar/painel/sem-nota`, { params: { pagination } }) },
             placeholderData: keepPreviousData
         }),
