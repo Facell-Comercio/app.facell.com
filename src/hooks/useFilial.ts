65636d2aa99dff271b3909e55637dfc4152c3427
jonathan <jonathanamarante@facell.com>
2024-06-19 08:37:38 -0300

    fix: uso async await em alguns hooks

--- src/hooks/useFilial.ts
+++ src/hooks/useFilial.ts
@@ -1,54 +1,79 @@
-import { api } from "@/lib/axios";
-import { keepPreviousData, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { GetAllParams } from "@/types/query-params-type";
-import { FilialFormData } from "@/pages/admin/filiais/filial/form-data";
 import { toast } from "@/components/ui/use-toast";
+import { api } from "@/lib/axios";
+import { FilialFormData } from "@/pages/admin/filiais/filial/form-data";
+import { GetAllParams } from "@/types/query-params-type";
+import {
+  keepPreviousData,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
 
-export const useFilial = ()=>{
-    const queryClient = useQueryClient()
-    return {
-    getAll: (params?: GetAllParams)=> useQuery({ 
-        queryKey: ['filiais', params], 
-        queryFn: async()=> await api.get('/filial', {params: params}), 
+export const useFilial = () => {
+  const queryClient = useQueryClient();
+  return {
+    getAll: (params?: GetAllParams) =>
+      useQuery({
+        queryKey: ["filiais", params],
+        queryFn: async () => await api.get("/filial", { params: params }),
         placeholderData: keepPreviousData,
-        staleTime: Infinity, 
-        refetchOnMount: false
-    }),
-    
-    getOne: (id?: string)=> useQuery({ 
-        enabled: !!id,
-        queryKey: ['filial', id], 
-        queryFn: async()=> await api.get(`/filial/${id}`), 
-        staleTime: Infinity, 
-        refetchOnMount: false
-    }),
+        staleTime: Infinity,
+        refetchOnMount: false,
+      }),
 
-    insertOne: () => useMutation({
-        mutationFn: (data: FilialFormData) => {
-            return api.post("filial", data).then((response) => response.data)
+    getOne: (id?: string) =>
+      useQuery({
+        enabled: !!id,
+        queryKey: ["filial", id],
+        queryFn: async () => await api.get(`/filial/${id}`),
+        staleTime: Infinity,
+        refetchOnMount: false,
+      }),
+
+    insertOne: () =>
+      useMutation({
+        mutationFn: async (data: FilialFormData) => {
+          return await api
+            .post("filial", data)
+            .then((response) => response.data);
         },
         onSuccess() {
-            toast({title: 'Sucesso!', description: 'Filial inserida com sucesso.'})
-            queryClient.invalidateQueries({ queryKey: ['filial'] })
+          toast({
+            title: "Sucesso!",
+            description: "Filial inserida com sucesso.",
+          });
+          queryClient.invalidateQueries({ queryKey: ["filial"] });
         },
         onError(error) {
-            toast({title: 'Ocorreu o seguinte erro', description: error.message})
-            console.log(error);
+          toast({
+            title: "Ocorreu o seguinte erro",
+            description: error.message,
+          });
+          console.log(error);
         },
-    }),
-    update: () => useMutation({
-        mutationFn: ({ id, ...rest }: FilialFormData) => {
-            return api.put("filial", { id, ...rest }).then((response) => response.data)
+      }),
+    update: () =>
+      useMutation({
+        mutationFn: async ({ id, ...rest }: FilialFormData) => {
+          return await api
+            .put("filial", { id, ...rest })
+            .then((response) => response.data);
         },
         onSuccess() {
-            toast({title: 'Sucesso!', description: 'Filial atualizada com sucesso.'})
-            queryClient.invalidateQueries({ queryKey: ['filiais'] })
-            queryClient.invalidateQueries({ queryKey: ['filial'] })
+          toast({
+            title: "Sucesso!",
+            description: "Filial atualizada com sucesso.",
+          });
+          queryClient.invalidateQueries({ queryKey: ["filiais"] });
+          queryClient.invalidateQueries({ queryKey: ["filial"] });
         },
         onError(error) {
-            toast({title: 'Ocorreu o seguinte erro', description: error.message})
-            console.log(error);
+          toast({
+            title: "Ocorreu o seguinte erro",
+            description: error.message,
+          });
+          console.log(error);
         },
-    })
-}
-}
+      }),
+  };
+};
--- src/hooks/usePermissoes.ts
+++ src/hooks/usePermissoes.ts
@@ -1,53 +1,64 @@
-
 import { api } from "@/lib/axios";
 import { Permissao } from "@/types/permissao-type";
 import { GetAllParams } from "@/types/query-params-type";
-import { keepPreviousData, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import {
+  keepPreviousData,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
 
 export const usePermissoes = () => {
-    const queryClient = useQueryClient()
-    return (
-        {
-            getAll : ({ pagination, filters }: GetAllParams) => useQuery({
-                queryKey: ['permissoes', pagination],
-                queryFn: async () => await api.get(`/permissao`, { params: { pagination, filters } }),
-                placeholderData: keepPreviousData
-            }),
-        
-            getOne : (id: string|null|undefined) => useQuery({
-                enabled: !!id,
-                queryKey: ['permissao', id],
-                queryFn: async () => {
-                    console.log(`Buscando título com base no ID: ${id}`)
-                    return await api.get(`/permissao/${id}`)
-                },
-            }),
-        
-            insertOne : () => useMutation({
-                mutationFn: (data:Permissao) => {            
-                    return api.post("/permissao", data).then((response)=>response.data)
-                },
-                onSuccess() {
-                    queryClient.invalidateQueries({queryKey:['permissoes']}) 
-                    queryClient.invalidateQueries({queryKey:['permissao']}) 
-                },
-                onError(error) {
-                    console.log(error);
-                },
-            }),
-        
-            update : () => useMutation({
-                mutationFn: ({id, ...rest}:Permissao) => {          
-                    return api.put("/permissao", {id, ...rest}).then((response)=>response.data)
-                },
-                onSuccess() {
-                    queryClient.invalidateQueries({queryKey:['permissoes']}) 
-                    queryClient.invalidateQueries({queryKey:['permissao']}) 
-                },
-                onError(error) {
-                    console.log(error);
-                },
-            }),        
-        }
-    )
-}
+  const queryClient = useQueryClient();
+  return {
+    getAll: ({ pagination, filters }: GetAllParams) =>
+      useQuery({
+        queryKey: ["permissoes", pagination],
+        queryFn: async () =>
+          await api.get(`/permissao`, { params: { pagination, filters } }),
+        placeholderData: keepPreviousData,
+      }),
+
+    getOne: (id: string | null | undefined) =>
+      useQuery({
+        enabled: !!id,
+        queryKey: ["permissao", id],
+        queryFn: async () => {
+          console.log(`Buscando título com base no ID: ${id}`);
+          return await api.get(`/permissao/${id}`);
+        },
+      }),
+
+    insertOne: () =>
+      useMutation({
+        mutationFn: async (data: Permissao) => {
+          return await api
+            .post("/permissao", data)
+            .then((response) => response.data);
+        },
+        onSuccess() {
+          queryClient.invalidateQueries({ queryKey: ["permissoes"] });
+          queryClient.invalidateQueries({ queryKey: ["permissao"] });
+        },
+        onError(error) {
+          console.log(error);
+        },
+      }),
+
+    update: () =>
+      useMutation({
+        mutationFn: async ({ id, ...rest }: Permissao) => {
+          return await api
+            .put("/permissao", { id, ...rest })
+            .then((response) => response.data);
+        },
+        onSuccess() {
+          queryClient.invalidateQueries({ queryKey: ["permissoes"] });
+          queryClient.invalidateQueries({ queryKey: ["permissao"] });
+        },
+        onError(error) {
+          console.log(error);
+        },
+      }),
+  };
+};
