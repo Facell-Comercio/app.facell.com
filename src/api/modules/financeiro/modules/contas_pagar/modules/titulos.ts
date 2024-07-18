10a4180c0157ad7a55f197788f6187feefdf0bdf
jonathan <jonathanamarante@facell.com>
2024-07-18 17:58:15 -0300

    feat: warning checkDoc

--- src/api/modules/financeiro/modules/contas_pagar/modules/titulos.ts
+++ src/api/modules/financeiro/modules/contas_pagar/modules/titulos.ts
@@ -1,6 +1,16 @@
-import { api } from "@/lib/axios";
+import { api } from '@/lib/axios';
 
-export const getAll = async (params:unknown) => {
-    const response = await api.get("/financeiro/contas-a-pagar/titulo", {params});
-    return response.data;
+export const getAll = async (params: unknown) => {
+  const response = await api.get('/financeiro/contas-a-pagar/titulo', {
+    params,
+  });
+  return response.data;
+};
+
+export const checkDoc = async (params: unknown) => {
+  const response = await api.get(
+    '/financeiro/contas-a-pagar/titulo/check-doc',
+    { params }
+  );
+  return response.data;
 };
