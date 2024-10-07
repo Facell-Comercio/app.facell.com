0e077f114f8c19d62ae100ebeaedf442aaf80dd3
Alex Bezerra <14alex25@gmail.com>
2024-10-07 14:17:34 -0300

    fix: correção p/ build

--- src/pages/financeiro/contas-pagar/titulos/components/ButtonExportarTitulos.tsx
+++ src/pages/financeiro/contas-pagar/titulos/components/ButtonExportarTitulos.tsx
@@ -82,7 +82,7 @@ const ButtonExportTitulos = () => {
         DESCRIÇÃO: row.descricao,
         VALOR: parseFloat(row.valor),
         FILIAL: row.filial,
-        FORNECEDOR: row.fornecedor,
+        FORNECEDOR: row.nome_fornecedor,
         "CNPJ FORNECEDOR": row.cnpj_fornecedor,
         SOLICITANTE: row.solicitante,
         "FORMA DE PAGAMENTO": row.forma_pagamento,
